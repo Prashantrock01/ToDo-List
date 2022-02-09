0e7261e795e4fdbb685b2220a9de782f348c3805
Prashant <prashantrock108@gmail.com>
2022-02-09 21:54:50 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import React from 'react';
+import ToDoList from './ToDoList';
 
 function App() {
   return (
    <> 
-   <h1> ToDo List</h1>
+   <>ToDoList</>
    </>
   );
 }
